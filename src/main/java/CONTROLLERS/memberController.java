2dc2cb05270b481d37276b49faa91e500a94cae7
임근혁 <lgh7586@naver.com>
2022-11-03 11:55:36 +0900

    ghim

---
+++ src/main/java/CONTROLLERS/memberController.java
@@ -0,0 +1,74 @@
+package CONTROLLERS;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.MembersDAO;
+
+@WebServlet("*.mem")
+public class memberController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF8");
+
+		String uri = request.getRequestURI();
+		System.out.println(uri);
+		try {
+			if (uri.equals("/login/complete.mem")) {
+				String id = request.getParameter("ID");
+				String passwd = request.getParameter("passwd");
+				String name = request.getParameter("nama");
+				String nickname = request.getParameter("nickname");
+				String number = request.getParameter("number");
+				String mail = request.getParameter("mail");
+				String zipcode = request.getParameter("zipcode");
+				String post1 = request.getParameter("address1");
+				String post2 = request.getParameter("address2");
+				SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+				String launch_date = format.format(System.currentTimeMillis());
+				System.out.println(launch_date);
+				MembersDAO dao = MembersDAO.getInstance();
+				int result = dao.insert(id, passwd, name, launch_date, nickname, 0, mail, number, zipcode, post1,
+						post2);
+				response.sendRedirect("/");
+			} else if (uri.equals("/login/login.mem")) {
+				String id = request.getParameter("ID");
+				String pwd = request.getParameter("passwd");
+				MembersDAO dao = MembersDAO.getInstance();
+				System.out.println(id + pwd);
+				boolean result = dao.login(id, pwd);
+				if (result) {
+					System.out.println("로그인 성공!");
+					request.getSession().setAttribute("loginID", id);
+					response.sendRedirect("/");
+				}
+			} else if (uri.equals("/login/duplCheck.mem")) {
+				String id = request.getParameter("ID");
+				MembersDAO dao = MembersDAO.getInstance();
+				boolean result = dao.isIDExist(id);
+				response.getWriter().append(String.valueOf(result));
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("error.html");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
