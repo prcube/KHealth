b6d5eab85ebfaff043832d7e24e25ce499c71e97
sdavids <sdavids@naver.com>
2022-11-07 10:57:55 +0900

    save

--- src/main/java/CONTROLLERS/ProductReviewController.java
+++ src/main/java/CONTROLLERS/ProductReviewController.java
@@ -1,16 +1,35 @@
 package CONTROLLERS;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.ProductReviewDAO;
+
 
 @WebServlet("*.review")
 public class ProductReviewController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uri = request.getRequestURI();
+		request.setCharacterEncoding("uft8");
+		
+		try {
+			
+			if(uri.equals("/.review")) {
+//				ProductReviewDAO dao = ProductReviewDAO.getInstance;
+				
+				
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 		
@@ -25,7 +44,6 @@ public class ProductReviewController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
