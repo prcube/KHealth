68bfd0ab431ac3a571ac150b32317dbf7cea973c
sdavids <sdavids@naver.com>
2022-11-08 17:29:30 +0900

    review CR complete

--- src/main/java/CONTROLLERS/ProductReviewController.java
+++ src/main/java/CONTROLLERS/ProductReviewController.java
@@ -50,7 +50,7 @@ public class ProductReviewController extends HttpServlet {
 				MemberDTO dto = dao2.selectById(writerid); 
 				ProductReviewDTO dto2 = new ProductReviewDTO(0,productseq,contents,writercharacter,writerid);
 				dao.insert(dto2);
-				request.getRequestDispatcher("/detail.market?product_seq="+productseq);
+				request.getRequestDispatcher("/detail.market?product_seq="+productseq).forward(request, response);
 			}
 			
 		}
