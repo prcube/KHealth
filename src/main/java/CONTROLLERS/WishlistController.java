82b6a0ae8ba9fa60b4b178ecb3b85e6be01939d1
LST <c3334943@naver.com>
2022-11-15 09:54:50 +0900

    market wishlist done

--- src/main/java/CONTROLLERS/WishlistController.java
+++ src/main/java/CONTROLLERS/WishlistController.java
@@ -43,38 +43,38 @@ public class WishlistController extends HttpServlet {
 			}
 		}
 
-		if(uri.equals("/add.wish")) {
-
-			try {
-				
-				int wishlist_seq = WishlistDAO.getInstance().getnextval();
-
-				String id = (String) request.getSession().getAttribute("loginID");
-				int amount = Integer.parseInt(request.getParameter("amount"));
-				int product_seq = Integer.parseInt(request.getParameter("product_seqForWishlist"));
-				String product_name = request.getParameter("product_nameForWishlist");
-				String product_price = request.getParameter("product_priceForWishlist");
-				
-				String oriName = ImagesDAO.getInstance().getImageOriName(product_seq);
-				System.out.println(product_seq);
-				boolean result = WishlistDAO.getInstance().isProductExist(id, product_name);
-				
-				if(result) {
-					WishlistDAO.getInstance().addupdate(amount, product_name, id);
-				}else {
-					WishlistDAO.getInstance().insert(wishlist_seq, product_name, product_seq, product_price,amount,id, oriName);
-				}
-				
-				System.out.println("삽입 완료");
-				
-				response.sendRedirect("/list.wish?cpage=1");
-
-			}catch(Exception e) {
-				e.printStackTrace();
-				response.sendRedirect("/error.jsp");
-			}
-
-		}
+//		if(uri.equals("/add.wish")) {
+//
+//			try {
+//				
+//				int wishlist_seq = WishlistDAO.getInstance().getnextval();
+//
+//				String id = (String) request.getSession().getAttribute("loginID");
+//				int amount = Integer.parseInt(request.getParameter("amount"));
+//				int product_seq = Integer.parseInt(request.getParameter("product_seqForWishlist"));
+//				String product_name = request.getParameter("product_nameForWishlist");
+//				String product_price = request.getParameter("product_priceForWishlist");
+//				
+//				String oriName = ImagesDAO.getInstance().getImageOriName(product_seq);
+//				System.out.println(product_seq);
+//				boolean result = WishlistDAO.getInstance().isProductExist(id, product_name);
+//				
+//				if(result) {
+//					WishlistDAO.getInstance().addupdate(amount, product_name, id);
+//				}else {
+//					WishlistDAO.getInstance().insert(wishlist_seq, product_name, product_seq, product_price,amount,id, oriName);
+//				}
+//				
+//				System.out.println("삽입 완료");
+//				
+//				response.sendRedirect("/list.wish?cpage=1");
+//
+//			}catch(Exception e) {
+//				e.printStackTrace();
+//				response.sendRedirect("/error.jsp");
+//			}
+//
+//		}
 		
 		if(uri.equals("/deleteall.wish")) {
 			String id = (String) request.getSession().getAttribute("loginID");
